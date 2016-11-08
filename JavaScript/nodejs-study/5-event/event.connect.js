a214568a4b4af0905d7b6f30a5ff097bdf9066e3
Yeongjun.Kim <yeongjun.kim@worksmobile.com>
2016-11-08 22:42:17 +0900

    update nodejs code

--- JavaScript/nodejs-study/5-event/event.connect.js
+++ JavaScript/nodejs-study/5-event/event.connect.js
@@ -1,17 +1,19 @@
-process.on('exit', function (code) {
-	console.log('good bye');
+process.on('exit', function(code) {
+    console.log('good bye');
 });
 
 process.on('uncaughtException', function() {
-	console.log('occured exception');	
+    console.log('occured exception');
 });
 
 var count = 0;
-var test = function () {
-	count = count + 1;
-	if (count > 3) { return; }
-	
-	setTimeout(test, 2000);
-	error.error.error();
+var test = function() {
+    count = count + 1;
+    if (count > 3) {
+        return;
+    }
+
+    setTimeout(test, 2000);
+    error.error.error();
 };
 setTimeout(test, 2000);
