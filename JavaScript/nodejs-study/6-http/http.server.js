d837a3ad02c308eeb0c735d5b5f90380dadb2c5e
Yeongjun.Kim <yeongjun.kim@worksmobile.com>
2016-11-09 14:03:56 +0900

    apply code formatter

--- JavaScript/nodejs-study/6-http/http.server.js
+++ JavaScript/nodejs-study/6-http/http.server.js
@@ -3,9 +3,9 @@ var server = require('http').createServer();
 
 // run server
 server.listen(52273, function() {
-	console.log('Server Running at http://127.0.0.1:52273');
+    console.log('Server Running at http://127.0.0.1:52273');
 });
 
 setTimeout(function() {
-	server.close();
+    server.close();
 }, 10000);
