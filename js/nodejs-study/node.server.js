3e8c6d0b3693adc5ebe60f9d6b5671d7ed9a2316
Yeongjun.Kim <yeongjun.kim@navercorp.com>
2019-07-29 11:02:27 +0900

    Delete nodejs directory

--- js/nodejs-study/node.server.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-
-http.createServer(function (request, response) {
-	response.writeHead(200, { 'Content-Type': 'text/html' });
-	response.end('<h1>Hello world!</h1>');
-}).listen(52273, function() {
-	console.log('Server running at http://127.0.0.1:52273/');
-});
