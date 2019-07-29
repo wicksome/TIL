aaea8c55b3a6f9a9a4245b7a815a8885124c4f7e
Yeongjun.Kim <yeongjun.kim@navercorp.com>
2019-07-29 11:02:27 +0900

    Delete nodejs directory

--- js/nodejs-study/08-express/express.middleware.static.js
+++
@@ -1,8 +0,0 @@
-var express = require('express'),
-    app = express();
-
-app.use(express.static(__dirname + '/public'))
-
-app.listen(52273, function() {
-    console.log('Test url', 'http://127.0.0.1:52273/tomcat.png');
-});
